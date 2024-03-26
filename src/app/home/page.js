4084926aa98dd16a8ea4be984215cd46a35e31c2
Ravi Shekhar <ravishekhar@Ravis-MacBook-Pro.local>
2024-03-26 21:20:57 +0530

    resized image

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,19 +1,31 @@
 import Image from "next/image";
+import AppBar from '@mui/material/AppBar';
+
+import Toolbar from '@mui/material/Toolbar';
+
+import Container from '@mui/material/Container';
 
 import './styles.css'
 const Home = (props) => {
     return(
         <div>
+            <AppBar position="static" sx={{ bgcolor: "white" }}>
+                <Container maxWidth="xl">
+                <Toolbar disableGutters></Toolbar>
+                </Container>
+            </AppBar>
             <Image
-                src="/hyundai-bg.jpg"
+                src="/hyundai.jpg"
                 layout="fill"
                 objectFit="contain"
                 quality={100}
                 className="bg-img"
                 priority
+                alt="Background Image of a vehicle"
             />
-            <div>
-                <p class="text-sm">what up</p>
+            <div class="content">
+                <h1 class="text-5xl pageHeading">Book your vehicle</h1> 
+                <h2 class="text-3xl description">It just doesn't take, it keeps the reservation too.</h2>
             </div>
         </div>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,11 +13,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "2.25rem",
+        "4xl": "2.5rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
     },
-    fontSize: {
-      '2xl': '1.5rem',
-      'sm': '3.5rem'
-    },
+
   },
   plugins: [],
 };
