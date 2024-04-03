b5faa1b8e3f2be58edd901ec3397f5c84b8ae3d6
Ravi Shekhar <ravishekhar@Ravis-MacBook-Pro.local>
2024-04-03 16:48:14 +0530

    fixed css for login button

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,10 +6,23 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 import './styles.css'
+import { useState } from 'react';
 
-const pages = ['Vehicles', 'Orders']
+const pages = ['Vehicles', 'Orders', 'All Bookings']
 
 const NavBar = (props) => {
+
+    const [isHovering, setHovering] = useState(false)
+
+    const onMouseEnter = () => {
+        console.log('called')
+        setHovering(true)
+    }
+
+    const onMouseLeave = () => {
+        setHovering(false)
+    }
+
     return (
         <AppBar position="static" sx={{ bgcolor: "white" }} elevation={0}>
             <Container maxWidth="xl">
@@ -39,20 +52,28 @@ const NavBar = (props) => {
                                 </Button>
                             ))}
                         </Box>
-                        <Box sx={{ display: 'flex', alignItems: 'center'  }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
                             <Button
                                 key="account"
-                                sx={{ my: 2, color: '#3730a3', display: 'block', fontWeight: 'bold' }}
+                                sx={{ my: 2, color: '#3730a3', display: 'flex', fontWeight: 'bold', alignItems: 'center', position: 'relative' }}
                                 className='login'
+                                onMouseEnter={onMouseEnter} 
+                                onMouseLeave={onMouseLeave}
                             >
-                                <Image
-                                    src="/locked.svg"
-                                    alt="login"
-                                    layout="fill"
-                                    objectFit="contain"
-                                    priority
-                                />
-                                Login/Acccount
+                                <span className={isHovering ? 'locked-icon' : 'locked-icon-hidden'}>
+                                    {console.log('isHovering', isHovering)}
+                                    {/* {isHovering &&  */}
+                                    <Image
+                                        src="/locked.svg"
+                                        alt="login"
+                                        objectFit="contain"
+                                        height={15}
+                                        width={15}
+                                        priority
+                                    />
+                                     {/* } */}
+                                </span>
+                                <span className='login-text'>Login/Acccount</span>
                             </Button>
                         </Box>
                     </Box>
